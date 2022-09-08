4e1dd6ea26bc2973440c00ab17727c68e47c48a1
Keyura Vadlamani <110452849+keyura99@users.noreply.github.com>
2022-09-08 12:04:31 +0530

    Time: 730 ms (17.61%), Space: 0B (100.00%) - LeetHub

---
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+SET N = N - 1; 
+  RETURN (
+      SELECT DISTINCT(Salary) FROM Employee
+      Union SELECT null 
+      ORDER BY Salary DESC LIMIT 1 OFFSET N
+  );
+END
