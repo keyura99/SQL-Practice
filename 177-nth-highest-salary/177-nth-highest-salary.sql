914089e4a99a5bab4b8ed04779760117668bdba4
Keyura Vadlamani <110452849+keyura99@users.noreply.github.com>
2022-09-08 12:07:56 +0530

    Update 177-nth-highest-salary.sql

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -7,3 +7,7 @@ SET N = N - 1;
       ORDER BY Salary DESC LIMIT 1 OFFSET N
   );
 END
+
+/* Here we are ordering our salary in descending order so we will get highest salary first and then subsequently lower salaries. 
+
+        The limit option allows you to limit the number of rows returned from a query, while offset allows you to omit a specified number of rows before the beginning of the result set */
